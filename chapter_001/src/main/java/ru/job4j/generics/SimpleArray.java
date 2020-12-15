0f90d4229dfcc800947e4504b0fe60e14490f110
Yuriy <CB.Phantom@ya.ru>
2020-12-15 14:13:56 +0300

    Реализовать SimpleArray<T> (оформление).

--- chapter_001/src/main/java/ru/job4j/generics/SimpleArray.java
+++ chapter_001/src/main/java/ru/job4j/generics/SimpleArray.java
@@ -4,23 +4,57 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
+/**
+ * В этом задании необходимо сделать универсальную обертку над массивом.
+ * Создать класс: public class SimpleArray<T>
+ * Добавить методы: add(T model), set(int index, T model), remove(int index), get(int index).
+ * Также, реализуйте интерфейс Iterable<T> - метод iterator() возвращает итератор,
+ * предназначенный для обхода данной структуры.
+ * В методах, где используется индекс нужно делать валидацию. Индекс должен находиться в рамках добавленных элементов.
+ * Для проверки индекса используйте метод Objects.checkIndex.
+ * Для удаления использовать System.arraycopy() вместо цикла.
+ *
+ * @param <T> Тип параметра.
+ */
 public class SimpleArray<T> implements Iterable<T> {
     private final Object[] data;
     private int index = 0;
 
+    /**
+     * Конструктор.
+     *
+     * @param capacity Принимает объём создаваемого объекта.
+     */
     public SimpleArray(int capacity) {
         this.data = new Object[capacity];
     }
 
+    /**
+     * Метод заменяет указанным элементом (model) элемент, находящийся по индексу index.
+     *
+     * @param index Входящий индекс ячейки массива.
+     * @param model Входящий элемент.
+     */
     public void set(int index, T model) {
         check(index);
         this.data[index] = model;
     }
 
+    /**
+     * Метод добавляет указанный элемент (model) в первую свободную ячейку.
+     *
+     * @param model Входящий элемент.
+     */
     public void add(T model) {
         this.data[index++] = model;
     }
 
+    /**
+     * Метод удаляет элемент по указанному индексу, все находящиеся справа элементы при этом необходимо
+     * сдвинуть на единицу влево (в середине массива не должно быть пустых ячеек).
+     *
+     * @param index Входящий индекс ячейки массива.
+     */
     public void remove(int index) {
         check(index);
         System.arraycopy(data, index + 1, data, index, data.length - 1 - index);
@@ -28,25 +62,49 @@ public class SimpleArray<T> implements Iterable<T> {
         this.index--;
     }
 
+    /**
+     * Метод возвращает элемент, расположенный по указанному индексу.
+     *
+     * @param index Входящий индекс ячейки массива.
+     * @return Элемент.
+     */
     public Object get(int index) {
         check(index);
         return this.data[index];
     }
 
+    /**
+     * Метод проверяет находится ли значение index п границах добавленных элементов.
+     *
+     * @param index Входящий индекс ячейки массива.
+     */
     public void check(int index) {
         Objects.checkIndex(index, this.index);
     }
 
+    /**
+     * Итератор.
+     *
+     * @return Новый итератор.
+     */
     @Override
     public Iterator<T> iterator() {
         return new Iterator<>() {
             private int pointer = 0;
 
+            /**
+             * Метод проверяет есть ли следующий элемент.
+             * @return Возвращает true если есть и false если нет.
+             */
             @Override
             public boolean hasNext() {
                 return pointer < index;
             }
 
+            /**
+             * Метод возвращает первый элемент.
+             * @return Элемент ячейки.
+             */
             @Override
             public T next() {
                 if (!hasNext()) {
--- chapter_001/src/test/java/ru/job4j/generics/SimpleArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -10,6 +10,9 @@ public class SimpleArrayTest {
     private final int capacity = 10;
     SimpleArray<Integer> array = new SimpleArray<>(capacity);
 
+    /**
+     * Инициализация массива до начала теста.
+     */
     @Before
     public void SetUp() {
         array.add(8);
@@ -18,41 +21,62 @@ public class SimpleArrayTest {
         array.add(33);
     }
 
+    /**
+     * Тест метода set.
+     */
     @Test
     public void whenUseSet() {
         array.set(3, 8);
         assertThat(array.get(3), is(8));
     }
 
+    /**
+     * Тест метода set при условии выхода за границы массива.
+     */
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenUseSetAndHaveIOOE() {
         array.set(10, 33);
         assertThat(array.get(10), is(33));
     }
 
+    /**
+     * Тест метода add.
+     */
     @Test
     public void whenUseAdd() {
         array.add(11);
         assertThat(array.get(4), is(11));
     }
 
+    /**
+     * Тест метода remove.
+     */
     @Test
     public void whenUseRemove() {
         array.remove(1);
         assertThat(array.get(1), is(9));
     }
 
+    /**
+     * Тест метода remove при условии выхода за границы массива.
+     */
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenUseRemoveAndHaveIOOE() {
         array.remove(10);
         assertThat(array.get(1), is(2));
     }
 
+    /**
+     * Тест метода get.
+     */
     @Test
     public void whenUseGet() {
         assertThat(9, is(array.get(2)));
     }
 
+    /**
+     * Тест метода get при условии выхода за границы массива.
+     */
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenUseGetAndHaveIOOE() {
         assertThat(2, is(array.get(10)));
