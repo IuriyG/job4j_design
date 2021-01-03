1d497582552ea489f091011c98dfda7339c70771
Yuriy <CB.Phantom@ya.ru>
2021-01-03 17:04:51 +0300

    Создать контейнер на базе связанного списка (оформление).

--- chapter_001/src/main/java/ru/job4j/collection/SimpleList.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleList.java
@@ -8,6 +8,15 @@ import java.util.Objects;
 /**
  * @author Iuriy Gaydarzhi.
  * @since 02.01.2021
+ * <p>
+ * Необходимо создать динамический контейнер с методами:
+ * 1) add(E value); (добавляет в конец);
+ * 2) E get(int index);
+ * 3) реализовать интерфейс Iterable<E>.
+ * Внутри контейнер должен базироваться на связанном списке(Node<E> node).
+ * Использовать стандартные коллекции JDK (ArrayList, LinkedList и т.д.) запрещено.
+ * Итератор должен реализовывать fail-fast поведение.
+ * В методах, где используется индекс нужно делать валидацию.
  */
 public class SimpleList<E> implements Iterable<E> {
     private int size = 0;
@@ -15,6 +24,11 @@ public class SimpleList<E> implements Iterable<E> {
     private Node<E> end;
     private int modCount;
 
+    /**
+     * Метод добавляет элемент в конец списка. Увеличивает размерность списка и счетчик изменений.
+     *
+     * @param value Элемент.
+     */
     public void add(E value) {
         Node<E> newList = new Node<>();
         newList.item = value;
@@ -28,28 +42,37 @@ public class SimpleList<E> implements Iterable<E> {
         this.modCount++;
     }
 
+    /**
+     * Метод возвращает элемент, расположенный по указанному индексу.
+     *
+     * @param index Индекс
+     * @return Элемент.
+     */
     public E get(int index) {
-        E resultData;
         checkIndex(index);
-        resultData = getIndex(index).item;
-        return resultData;
-    }
-
-    public Node<E> getIndex(int position) {
-        Node<E> result;
-        result = this.head;
-        for (int i = 0; i < position; i++) {
+        Node<E> result = this.head;
+        for (int i = 0; i < index; i++) {
             if (result.next != null) {
                 result = result.next;
             }
         }
-        return result;
+        return result.item;
     }
 
+    /**
+     * Метод проверяет находится ли значение index в границах добавленных элементов.
+     *
+     * @param index Индекс.
+     */
     public void checkIndex(int index) {
         Objects.checkIndex(index, this.size);
     }
 
+    /**
+     * Итератор.
+     *
+     * @return Новый итератор.
+     */
     @Override
     public Iterator<E> iterator() {
 
@@ -57,6 +80,11 @@ public class SimpleList<E> implements Iterable<E> {
             private final int expectedModCount = modCount;
             private int pointer = 0;
 
+            /**
+             * Метод проверяет есть ли следующий элемент.
+             * Реализовано fail-fast поведение.
+             * @return Возвращает true если есть и false если нет.
+             */
             @Override
             public boolean hasNext() {
                 if (expectedModCount != modCount) {
@@ -65,6 +93,10 @@ public class SimpleList<E> implements Iterable<E> {
                 return pointer < size;
             }
 
+            /**
+             * Метод возвращает первый элемент.
+             * @return Элемент.
+             */
             @Override
             public E next() {
                 if (!hasNext()) {
@@ -78,6 +110,11 @@ public class SimpleList<E> implements Iterable<E> {
         };
     }
 
+    /**
+     * Модель для связанного списка.
+     *
+     * @param <E> Элемент.
+     */
     private static class Node<E> {
         protected E item;
         protected Node<E> next;
