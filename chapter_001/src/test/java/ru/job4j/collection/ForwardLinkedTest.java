b6954bbc0483862d3d4249ebb853cbfddf35fc3b
Yuriy <CB.Phantom@ya.ru>
2021-01-04 23:59:22 +0300

    Удалить head в односвязном списке.

---
+++ chapter_001/src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.collection;
+
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 04.01.2021
+ */
+
+public class ForwardLinkedTest {
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteFirst() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.deleteFirst();
+        linked.iterator().next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteEmptyLinked() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.deleteFirst();
+    }
+
+    @Ignore
+    @Test
+    public void whenMultiDelete() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        assertThat(linked.deleteFirst(), is(1));
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+    }
+}
