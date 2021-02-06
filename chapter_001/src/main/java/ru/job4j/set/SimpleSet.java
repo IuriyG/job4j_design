63579c204c96bde1af3c7c2a50a5e3b64b950ed9
Yuriy <CB.Phantom@ya.ru>
2021-02-06 21:07:04 +0300

    Реализовать коллекцию Set на массиве (оформление).

--- chapter_001/src/main/java/ru/job4j/set/SimpleSet.java
+++ chapter_001/src/main/java/ru/job4j/set/SimpleSet.java
@@ -8,12 +8,23 @@ import java.util.Objects;
 /**
  * @author Iuriy Gaydarzhi.
  * @since 01.02.2021
+ *
+ * <b>1. Реализовать коллекцию Set на массиве</b>
+ * 1. Реализовать коллекцию SimpleSet. Коллекция должна обеспечивать boolean add(E e) и реализовывать Iterable<E>.
+ * 2. Написать тесты на наличие дубликатов.
+ * 3. Написать тест с учетом null элемента.
  */
 public class SimpleSet<E> implements Iterable<E> {
     private final SimpleArray<E> simpleArray = new SimpleArray<>(10);
 
+    /**
+     * Метод добавляет элементы в список simpleArray.
+     *
+     * @param e Входящий элемент.
+     * @return Возвращает true если элемент добавлен или false если нет.
+     */
     public boolean add(E e) {
-        if (contains(e)) {
+        if (checkForDuplicates(e)) {
             simpleArray.add(e);
             return true;
         } else {
@@ -21,7 +32,13 @@ public class SimpleSet<E> implements Iterable<E> {
         }
     }
 
-    public boolean contains(E e) {
+    /**
+     * Метод проверяет список на отсутствие дубликатов.
+     *
+     * @param e Входящий элемент.
+     * @return Возвращает true если если нет дубликатов и false если найден.
+     */
+    public boolean checkForDuplicates(E e) {
         boolean rsl = true;
         for (E e1 : simpleArray) {
             if (Objects.equals(e1, e)) {
@@ -32,6 +49,11 @@ public class SimpleSet<E> implements Iterable<E> {
         return rsl;
     }
 
+    /**
+     * Итератор.
+     *
+     * @return Возвращает итератор класса SimpleArray.
+     */
     @Override
     public Iterator<E> iterator() {
         return simpleArray.iterator();
