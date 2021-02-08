09f0eefdc9b781528e65eba56ae88bda64573ff1
Yuriy <CB.Phantom@ya.ru>
2021-02-09 00:59:12 +0300

    Создать модель User.

---
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,16 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 09.02.2021
+ *
+ * <b>1. Создать модель User</b>
+ * Создать модель User и три поля.
+ */
+public class User {
+    Calendar birthday;
+    private String name;
+    private int children;
+}
