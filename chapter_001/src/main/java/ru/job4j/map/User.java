8dd57365eb1dca9f39d93c70ebeef05a4f23bc32
Yuriy <CB.Phantom@ya.ru>
2021-02-09 02:44:12 +0300

    Без переопределения equals и hashCode.

--- chapter_001/src/main/java/ru/job4j/map/User.java
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Iuriy Gaydarzhi.
@@ -10,7 +13,29 @@ import java.util.Calendar;
  * Создать модель User и три поля.
  */
 public class User {
-    Calendar birthday;
-    private String name;
-    private int children;
+    private final Calendar birthday;
+    private final String name;
+    private final int children;
+
+    public User(Calendar birthday, String name, int children) {
+        this.birthday = birthday;
+        this.name = name;
+        this.children = children;
+    }
+
+    public static void main(String[] args) {
+        Calendar calendar = new GregorianCalendar(2001, Calendar.SEPTEMBER, 9);
+        User user = new User(calendar, "Ann", 19);
+        User user2 = new User(calendar, "Ann", 19);
+        Map<User, Object> map = new HashMap<>();
+        map.put(user, new Object());
+        map.put(user2, new Object());
+
+        for (Map.Entry<User, Object> entry : map.entrySet()) {
+            User u = entry.getKey();
+            Object o = entry.getValue();
+            System.out.println(u);
+            System.out.println(o);
+        }
+    }
 }
