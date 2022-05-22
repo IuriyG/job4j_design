4917c59723b63c5e6a07a6b1959f4aa157576b87
Iuriy <IuriyG@protonmail.com>
2022-05-23 00:38:01 +0300

    BufferedReader (корректировка).

--- chapter_002/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_002/src/main/java/ru/job4j/io/LogFilter.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -21,10 +22,9 @@ public class LogFilter {
     public List<String> filter(String file) {
         List<String> list = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            for (String line = in.readLine(); line != null; line = in.readLine()) {
-                if (line.substring(line.lastIndexOf('"'), line.lastIndexOf(' ')).contains("404")) {
-                    list.add(line);
-                }
+            String read;
+            while ((read = in.readLine()) != null) {
+                Arrays.stream(read.split(" ")).filter(s -> s.matches("404")).forEach(list::add);
             }
         } catch (IOException e) {
             e.printStackTrace();
