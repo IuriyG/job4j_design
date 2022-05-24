f83156420330ae0aca993269ece4e5de3721c63b
Iuriy <IuriyG@protonmail.com>
2022-05-24 20:15:29 +0300

    BufferedOutputStream.

--- chapter_002/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_002/src/main/java/ru/job4j/io/LogFilter.java
@@ -1,8 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +20,21 @@ public class LogFilter {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
         log.forEach(System.out::println);
+        save(log, "404.txt");
+    }
+
+    /**
+     * Метод записывает данные из переданного файла в другой переданный файл.
+     *
+     * @param log  Входящий аргумент.
+     * @param file Входящий аргумент.
+     */
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+            log.forEach(out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
