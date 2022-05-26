6c62ec19a51ec4191a920c18c07de21558794f21
Iuriy <IuriyG@protonmail.com>
2022-05-26 16:06:01 +0300

    Читаем файл конфигурации.

---
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,57 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 25.05.2022
+ */
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+
+    public void load() {
+        try (BufferedReader br = new BufferedReader(new FileReader(this.path))) {
+            br.lines().filter(s -> s.contains("=")).forEach(s -> {
+                String[] keyValuePair = s.split("=", 2);
+                String key = keyValuePair[0];
+                String value = keyValuePair[1];
+                if (value.isEmpty() || key.isEmpty()) {
+                    throw new IllegalArgumentException();
+                }
+                values.put(key, value);
+            });
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 25.05.2022
+ */
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("name"), "James Gosling");
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("name"), "James Gosling");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenValueEmptyWithComment() {
+        String path = "./data/value_empty_comment.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenKeyIsEmpty() {
+        String path = "./data/key_empty.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNoSplitSymbol() {
+        String path = "./data/empty.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+   @Test
+    public void whenManySplitSymbols() {
+        String path = "./data/several_symbols.properties";
+        Config config = new Config(path);
+        config.load();
+       assertEquals(config.value("name"), "author=1=James Gosling");
+   }
+}
