bdeb10b5d96eae482f48211f80965bf5a2b453d8
Iuriy <IuriyG@protonmail.com>
2022-05-31 14:37:09 +0300

    Читаем файл конфигурации (Описание).

--- chapter_002/src/main/java/ru/job4j/io/Config.java
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -10,20 +10,41 @@ import java.util.StringJoiner;
 /**
  * @author Iuriy Gaydarzhi.
  * @since 25.05.2022
+ * <p>
+ * Класс считывает данные из строкового файла, добавляет из в коллекцию и возвращает значение по ключу.
  */
 public class Config {
-
+    /**
+     * Файл.
+     */
     private final String path;
+    /**
+     * Коллекция значений.
+     */
     private final Map<String, String> values = new HashMap<>();
 
+    /**
+     * Конструктор.
+     *
+     * @param path Входящий аргумент.
+     */
     public Config(final String path) {
         this.path = path;
     }
 
+    /**
+     * Основной класс.
+     *
+     * @param args Входящий аргумент.
+     */
     public static void main(String[] args) {
         System.out.println(new Config("app.properties"));
     }
 
+    /**
+     * Метод принимает файл строк, считывает его пропуская строку, которая начинается с символа '#' (комментарии).
+     * И добавляет в коллекцию пару: ключ - значение.
+     */
     public void load() {
         try (BufferedReader br = new BufferedReader(new FileReader(this.path))) {
             br.lines().filter(s -> !s.startsWith("#")).filter(s -> s.contains("=")).forEach(s -> {
@@ -40,10 +61,21 @@ public class Config {
         }
     }
 
+    /**
+     * Сравнивает входящий ключ с ключом из коллекции.
+     *
+     * @param key Входящий аргумент.
+     * @return Возвращает значение по ключу.
+     */
     public String value(String key) {
         return values.get(key);
     }
 
+    /**
+     * Переопределенный метод.
+     *
+     * @return Возвращает строки в читабельном виде.
+     */
     @Override
     public String toString() {
         StringJoiner out = new StringJoiner(System.lineSeparator());
