e3474766a07ef563937d3badd2557a17d62b5d00
Iuriy <IuriyG@protonmail.com>
2022-06-02 19:37:57 +0300

    File.

---
+++ chapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.util.Objects;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 02.06.2022
+ */
+public class Dir {
+
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        for (File subfile : Objects.requireNonNull(file.listFiles())) {
+            if (subfile.isFile()) {
+                System.out.printf("Name: %s%n", subfile.getName() + " Size: " + subfile.length());
+            }
+        }
+    }
+}
