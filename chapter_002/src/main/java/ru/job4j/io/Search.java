acadff0aa03b8b85196c89e073098fec07985002
Iuriy <IuriyG@protonmail.com>
2022-06-11 14:23:50 +0300

    Валидация параметров запуска (Комментарии).

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -9,22 +9,47 @@ import java.util.List;
 import java.util.function.Predicate;
 
 /**
+ * Класс осуществляет поиск файлов по определённому предикату.
+ * Также проводит валидацию входящих аргументов.
+ *
  * @author Iuriy Gaydarzhi.
  * @since 04.06.2022
  */
 public class Search {
+    /**
+     * Основной метод.
+     *
+     * @param args Входящие аргументы.
+     * @throws IOException Исключение ввода-вывода.
+     */
     public static void main(String[] args) throws IOException {
         Path start = Paths.get("c://projects//job4j_design");
         validateFiles(new String[]{String.valueOf(start), ".txt"});
         search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out::println);
     }
 
+    /**
+     * Метод проходит по заданному каталогу 'root' и подкаталогам соблюдая условие 'condition'.
+     *
+     * @param root      Корневая папка.
+     * @param condition Условия для поиска файла.
+     * @return Возвращает список соответствующих файлов.
+     * @throws IOException Исключение ввода-вывода.
+     */
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
 
+    /**
+     * Метод проверяет входящие аргументы по следующим критериям:
+     * наличие двух входящих аргументов;
+     * является ли первый аргумент директорией;
+     * начинается ли второй аргумент с точки.
+     *
+     * @param args Входящие аргументы.
+     */
     public static void validateFiles(String[] args) {
         if (args.length != 2) {
             throw new IllegalArgumentException("Требуется два аргумента!");
@@ -37,4 +62,3 @@ public class Search {
         }
     }
 }
-
--- chapter_002/src/main/java/ru/job4j/io/SearchFiles.java
+++ chapter_002/src/main/java/ru/job4j/io/SearchFiles.java
@@ -12,22 +12,49 @@ import java.util.function.Predicate;
 import static java.nio.file.FileVisitResult.CONTINUE;
 
 /**
+ * Класс описывает логику поиска фалов при прохождении всей директории включая вложенные.
+ *
  * @author Iuriy Gaydarzhi.
  * @since 04.06.2022
  */
 public class SearchFiles implements FileVisitor<Path> {
+    /**
+     * Поле предиката.
+     */
     private final Predicate<Path> condition;
+    /**
+     * Поле для списка файлов.
+     */
     private final List<Path> list = new ArrayList<>();
 
+    /**
+     * Конструктор.
+     *
+     * @param condition Возвращает предикат.
+     */
     public SearchFiles(Predicate<Path> condition) {
         this.condition = condition;
     }
 
+    /**
+     * Метод продолжает обход.
+     *
+     * @param dir   a reference to the directory
+     * @param attrs the directory's basic attributes
+     * @return Продолжает обход.
+     */
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
         return CONTINUE;
     }
 
+    /**
+     * Метод проходит по каталогам, добавляя в список файлы, которые соответствуют условиям предиката.
+     *
+     * @param file  a reference to the file
+     * @param attrs the file's basic attributes
+     * @return Продолжает обход.
+     */
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
         if (condition.test(file)) {
@@ -36,16 +63,37 @@ public class SearchFiles implements FileVisitor<Path> {
         return CONTINUE;
     }
 
+    /**
+     * Метод продолжает обход.
+     *
+     * @param file a reference to the file
+     * @param exc  the I/O exception that prevented the file from being visited
+     * @return Продолжает обход.
+     */
     @Override
     public FileVisitResult visitFileFailed(Path file, IOException exc) {
         return CONTINUE;
     }
 
+    /**
+     * Метод продолжает обход.
+     *
+     * @param dir a reference to the directory
+     * @param exc {@code null} if the iteration of the directory completes without
+     *            an error; otherwise the I/O exception that caused the iteration
+     *            of the directory to complete prematurely
+     * @return Продолжает обход.
+     */
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
         return CONTINUE;
     }
 
+    /**
+     * Конструктор для поля.
+     *
+     * @return Возвращает результирующий список.
+     */
     public List<Path> getPaths() {
         return list;
     }
