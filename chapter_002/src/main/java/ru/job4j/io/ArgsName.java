bd23e59875c5676e22764352f25dd3b67d193f2c
Iuriy <IuriyG@protonmail.com>
2022-06-16 18:30:03 +0300

    Именованные аргументы.

---
+++ chapter_002/src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,66 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 15.06.2022
+ */
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+
+    private void validateData(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Входящий массив пуст!");
+        }
+        for (String arg : args) {
+            if (!arg.contains("=")) {
+                throw new IllegalArgumentException("Отсутствует разделяющий символ - \"=\"!");
+            }
+            String[] keyValuePair = arg.split("=", 2);
+            String key = keyValuePair[0];
+            String value = keyValuePair[1];
+            if (!key.startsWith("-")) {
+                throw new IllegalArgumentException("Отсутствует первый символ ключа - \"-\"");
+            }
+            if (key.equals("-")) {
+                throw new IllegalArgumentException("Присутствует только первый символ ключа!");
+            }
+            if (value.isEmpty()) {
+                throw new IllegalArgumentException("Отсутствует значение ключа!");
+            }
+        }
+    }
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("Отсутствует ключ!");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        validateData(args);
+        for (String arg : args) {
+            String[] keyValuePair = arg.split("=", 2);
+            String key = keyValuePair[0].substring(1);
+            String value = keyValuePair[1];
+            values.put(key, value);
+        }
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/io/ArgsNameTest.java
@@ -0,0 +1,78 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 15.06.2022
+ */
+public class ArgsNameTest {
+
+    @Test
+    public void whenGetFirst() {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        assertEquals(jvm.get("Xmx"), "512");
+    }
+
+    @Test
+    public void whenGetFirstReorder() {
+        ArgsName jvm = ArgsName.of(new String[]{"-encoding=UTF-8", "-Xmx=512"});
+        assertEquals(jvm.get("Xmx"), "512");
+    }
+
+    @Test
+    public void whenGetTwoArgumentsInReversOrder() {
+        ArgsName jvm = ArgsName.of(new String[]{"-encoding=UTF-8", "-Xmx=512"});
+        assertEquals(jvm.get("Xmx"), "512");
+        assertEquals(jvm.get("encoding"), "UTF-8");
+    }
+
+    @Test
+    public void whenMultipleEqualsSymbol() {
+        ArgsName jvm = ArgsName.of(new String[]{"-request=?msg=Exit="});
+        assertEquals(jvm.get("request"), "?msg=Exit=");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenGetNotExist() {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512"});
+        jvm.get("Xms");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenWrongSomeArgument() {
+        ArgsName jvm = ArgsName.of(new String[]{"-encoding=UTF-8", "-Xmx="});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenArrayIsEmpty() {
+        ArgsName jvm = ArgsName.of(new String[]{});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenKeyIsHyphen() {
+        ArgsName jvm = ArgsName.of(new String[]{"-=512"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenAbsentEqualSymbol() {
+        ArgsName jvm = ArgsName.of(new String[]{"-encodingUTF-8"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenAbsentHyphenSymbol() {
+        ArgsName jvm = ArgsName.of(new String[]{"encoding=UTF-8"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenAbsentKeyAndTwoEqualSymbol() {
+        ArgsName jvm = ArgsName.of(new String[]{"-==password"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenAbsentKeyAndValue() {
+        ArgsName jvm = ArgsName.of(new String[]{"-="});
+    }
+}
