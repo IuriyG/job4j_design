9d1af12be784163ac92a33a4e7d179df4320c2c8
Iuriy <IuriyG@protonmail.com>
2022-06-22 20:33:34 +0300

    Архивировать проект (добавил комментарии).

--- chapter_002/src/main/java/ru/job4j/io/Zip.java
+++ chapter_002/src/main/java/ru/job4j/io/Zip.java
@@ -9,14 +9,46 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 /**
+ * Класс архивирует папку и все содержимое: файлы, подкаталоги, всё, кроме файлов-исключений.
+ * Исключения, задается в параметрах.
+ * Параметры для запуска метода: -d=C:\projects\job4j_design -e=.java -o=project.zip
+ * Значение ключей:
+ * -d - директория, которую мы хотим архивировать.
+ * -e - исключаем файлы с расширением - '.java'.
+ * -o - название конечного архива.
+ *
  * @author Iuriy Gaydarzhi.
  * @since 20.06.2022
  */
 public class Zip {
+    /**
+     * Директория с которой начнется архивирование.
+     */
     private static final String DIR = "d";
+    /**
+     * Исключаемые файлы.
+     */
     private static final String EXC = "e";
+
+    /**
+     * Конечный архив.
+     */
     private static final String OUT = "o";
 
+    /**
+     * Основной метод.
+     * Проверяет наличие трёх аргументов во входящих данных.
+     * Архивирует один файл с заданными данными.
+     * Добавляет входные данные в коллекцию.
+     * Возвращает все три аргумента в нужном формате.
+     * Проводит валидацию входных данных.
+     * Создает предикат и необходимый формат для дальнейшего использования.
+     * Создает список файлов для архиваций.
+     * Создает необходимый нам архив.
+     *
+     * @param args Входящие аргументы. Установлены в параметрах запуска метода 'main'.
+     * @throws IOException Исключение ввода-вывода.
+     */
     public static void main(String[] args) throws IOException {
         if (args.length != 3) {
             throw new IllegalArgumentException("Аргументов должно быть три!");
@@ -34,6 +66,14 @@ public class Zip {
         zip.packFiles(list, output);
     }
 
+    /**
+     * Метод проводит валидацию входных данных:
+     * существует ли архивируемая директория;
+     * начинается ли расширения исключаемого файла с точки;
+     * название конечного файла должен содержать - '.zip'.
+     *
+     * @param args Входящие данные.
+     */
     private void validateArgs(ArgsName args) {
         Path in = Paths.get(args.get(DIR));
         if (!in.toFile().isDirectory()) {
@@ -47,6 +87,12 @@ public class Zip {
         }
     }
 
+    /**
+     * Метод считывает и архивирует файлы.
+     *
+     * @param sources Директория и исключаемые файлы.
+     * @param target  Конечный архив.
+     */
     public void packFiles(List<Path> sources, Path target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.toFile())))) {
             for (Path path : sources) {
@@ -62,6 +108,12 @@ public class Zip {
 
     }
 
+    /**
+     * Метод считывает и архивирует один файл.
+     *
+     * @param source Директория и исключаемые файлы.
+     * @param target Конечный архив.
+     */
     public void packSingleFile(File source, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             zip.putNextEntry(new ZipEntry(source.getPath()));
