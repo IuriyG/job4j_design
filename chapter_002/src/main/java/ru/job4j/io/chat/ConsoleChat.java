37108502937ff88480bfce962d8e47c95aa53b92
Iuriy <IuriyG@protonmail.com>
2022-06-26 20:54:54 +0300

    Кодировка (Комментарии).

--- chapter_002/src/main/java/ru/job4j/io/chat/ConsoleChat.java
+++ chapter_002/src/main/java/ru/job4j/io/chat/ConsoleChat.java
@@ -6,13 +6,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Класс имитирует работу консольного чата.
+ *
  * @author Iuriy Gaydarzhi.
  * @since 23.06.2022
  */
 public class ConsoleChat {
+    /**
+     * Команда для бота. При вводе слова, программа прекращает работу.
+     */
     private static final String OUT = "закончить";
+    /**
+     * Команда для бота. При вводе слова, бот замолкает.
+     */
     private static final String STOP = "стоп";
+    /**
+     * Команда для бота. При вводе слова, бот продолжает работать.
+     */
     private static final String CONTINUE = "продолжить";
+    /**
+     * Список для временного хранилища диалога.
+     */
     private final List<String> list = new ArrayList<>();
     /**
      * Лог диалога между пользователем и ботом.
@@ -23,16 +37,32 @@ public class ConsoleChat {
      */
     private final String botAnswers;
 
+    /**
+     * Конструктор.
+     */
     public ConsoleChat(String path, String botAnswers) {
         this.path = path;
         this.botAnswers = botAnswers;
     }
 
+    /**
+     * Основной метод. Демонстрирует работу метода run.
+     *
+     * @param args Взводящие аргументы.
+     * @throws IOException Ошибка при вводе-выводе данных.
+     */
     public static void main(String[] args) throws IOException {
         ConsoleChat cc = new ConsoleChat("./chapter_002/data/bot&user_log.txt", "./chapter_002/data/sentence_for_bot.txt");
         cc.run();
     }
 
+    /**
+     * Метод считывает фразы пользователя из консоли. Затем отвечает случайными предложениями из списка.
+     * Если пользователь вводит слово 'стоп', бот умолкает пока не будет введено ключевое слово 'продолжить'.
+     * Если ввести слово 'закончить' программа прекратит работу.
+     *
+     * @throws IOException Ошибка при вводе-выводе данных.
+     */
     public void run() throws IOException {
         List<String> randomSentence = readPhrases();
         String you = "Вы:";
@@ -44,16 +74,16 @@ public class ConsoleChat {
         boolean execute = true;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println(talk);
-        list.add(talk);
+        this.list.add(talk);
         do {
             System.out.println(you);
             word = br.readLine();
             this.list.add(you + System.lineSeparator() + word);
             if (STOP.equals(word)) {
                 System.out.println(bot);
-                list.add(bot);
+                this.list.add(bot);
                 System.out.println(answer);
-                list.add(answer);
+                this.list.add(answer);
                 while (execute) {
                     word = br.readLine();
                     this.list.add(word);
@@ -64,11 +94,11 @@ public class ConsoleChat {
                 execute = true;
             } else if (OUT.equals(word)) {
                 System.out.println(bye);
-                list.add(bye);
+                this.list.add(bye);
                 break;
             }
             System.out.println(bot);
-            list.add(bot);
+            this.list.add(bot);
             double random = Math.random() * 50;
             System.out.println(randomSentence.get((int) random));
             this.list.add(randomSentence.get((int) random));
@@ -76,6 +106,11 @@ public class ConsoleChat {
         saveLog(this.list);
     }
 
+    /**
+     * Метод считывает предложения из файла и добавляет из в список.
+     *
+     * @return Список с фразами.
+     */
     private List<String> readPhrases() {
         List<String> list = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(this.botAnswers, StandardCharsets.UTF_8))) {
@@ -86,6 +121,11 @@ public class ConsoleChat {
         return list;
     }
 
+    /**
+     * Метод записывает в файл диалог пользователя и бота в соответствующем порядке.
+     *
+     * @param log Принимает список, временное хранилище.
+     */
     private void saveLog(List<String> log) {
         try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(this.path)))) {
             log.forEach(out::println);
