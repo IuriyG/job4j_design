a6f764cdc0a5b19fe8d8cb23c98c1ed629f4cf47
Iuriy <IuriyG@protonmail.com>
2022-07-03 01:24:35 +0300

    Scanner (Добавил комментарии).

--- chapter_002/src/main/java/ru/job4j/io/scanner/CSVReader.java
+++ chapter_002/src/main/java/ru/job4j/io/scanner/CSVReader.java
@@ -25,18 +25,45 @@ public class CSVReader {
      */
     private final List<String> result = new ArrayList<>();
     /**
-     * Разделитель.
+     * Разделитель. Делит аргумент 'filter' на две части.
      */
     private String[] toggle;
+    /**
+     * Файл с исходными данными.
+     */
     private String file;
+    /**
+     * Аргумент 'delimiter'.
+     * Разделитель. Служит разделителем между словами в результирующем списке или в консоли.
+     */
     private String delimiter;
+    /**
+     * Аргумент 'out'.
+     * Конечный файл с результирующими данными или вывод в консоль.
+     */
     private String target;
 
+    /**
+     * Основной метод.
+     *
+     * @param args Входящие аргументы.
+     * @throws FileNotFoundException Ошибка при вводе-выводе данных.
+     */
     public static void main(String[] args) throws FileNotFoundException {
         CSVReader csvReader = new CSVReader();
         csvReader.handle(ArgsName.of(args));
     }
 
+    /**
+     * Метод считывает входящие аргументы и назначает их переменным.
+     * Вызывает метод {@link #validateKeys()}.
+     * Считывает исходный файл добавляя данные во временный список.
+     * Вызывает метод 'separateArgs'.
+     * Далее зависимости от аргумента {@link #target}, выводит в консоль или взывает метод {@link #separateArgs(List)}.
+     *
+     * @param argsName Входящие аргументы.
+     * @throws FileNotFoundException Ошибка при отсутствии файла.
+     */
     public void handle(ArgsName argsName) throws FileNotFoundException {
         file = argsName.get("path");
         delimiter = argsName.get("delimiter");
@@ -57,6 +84,15 @@ public class CSVReader {
         }
     }
 
+    /**
+     * Метод создает временный список в котором находятся только фильтры.
+     * Затем находит индекс перового и второго аргумента. Далее присваивает их соответствующим временным переменным.
+     * В цикле считывает строку из входящего списка и передает во временный список,
+     * назначая каждому слову разделённому символом ';' свой индекс.
+     * Потом используя временные переменные в качестве индекса добавляет слова в новый список,
+     * разделяя их {@link #delimiter}.
+     * @param args Список с данными.
+     */
     private void separateArgs(List<String> args) {
         List<String> strings = new ArrayList<>(List.of(args.get(0).split(";")));
         int fArg = strings.indexOf(toggle[0]);
@@ -67,6 +103,15 @@ public class CSVReader {
         }
     }
 
+    /**
+     * Метод проводит валидацию данных согласно условию задания:
+     * расширение исходного файла должно быть - '.csv';
+     * аргумент 'delimiter', должен быть указан как - ';';
+     * аргумент 'out' имеет только два возможный вариантна:
+     * 1. Для вывода в консоль указать в качестве параметра - 'stdout';
+     * 2. Для записи в файл, указать название файла с таким же расширением, как и источник;
+     * Количество фильтров должно быть два.
+     */
     private void validateKeys() {
         if (!new File(this.file).getName().endsWith(".csv")) {
             throw new IllegalArgumentException("Аргумент 'path' указан не верно!");
@@ -83,6 +128,10 @@ public class CSVReader {
         }
     }
 
+    /**
+     * Метод записывает результирующий список {@link #target} в файл {@link #target}.
+     * @param log Список с данными.
+     */
     private void resultLog(List<String> log) {
         try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(this.target)))) {
             log.forEach(out::println);
