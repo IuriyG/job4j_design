c83e1ac113d03a5429670e357e12548792b07d79
Iuriy <iuriyG@proton.me>
2022-08-21 22:09:51 +0300

    Утверждения с коллекциями.

---
+++ chapter_001/src/main/java/ru/job4j/assertj/SimpleConvert.java
@@ -0,0 +1,36 @@
+package ru.job4j.assertj;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 21.08.2022
+ */
+public class SimpleConvert {
+    public String[] toArray(String... example) {
+        return example;
+    }
+
+    public List<String> toList(String... example) {
+        return Arrays.stream(example).toList();
+    }
+
+    public Set<String> toSet(String... example) {
+        return Arrays.stream(example).collect(Collectors.toSet());
+    }
+
+    public Map<String, Integer> toMap(String... example) {
+        AtomicInteger i = new AtomicInteger();
+        return Arrays.stream(example)
+                .collect(Collectors.toMap(
+                        e -> e,
+                        e -> i.getAndIncrement(),
+                        (first, second) -> first)
+                );
+    }
+}
